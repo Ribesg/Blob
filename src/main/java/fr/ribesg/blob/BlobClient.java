ec11165f6f69f2753403f1f9c242a82e5731de3b
Ribesg <ribesg@yahoo.fr>
2014-03-20 14:33:29 +0100

    Some edits for testing and to match Alix

--- src/main/java/fr/ribesg/blob/BlobClient.java
+++ src/main/java/fr/ribesg/blob/BlobClient.java
@@ -4,7 +4,6 @@ import fr.ribesg.alix.api.Client;
 import fr.ribesg.alix.api.Server;
 import fr.ribesg.alix.api.bot.command.CommandManager;
 import fr.ribesg.alix.api.enums.Codes;
-import fr.ribesg.alix.api.message.NickIrcPacket;
 import fr.ribesg.alix.api.network.ssl.SSLType;
 import fr.ribesg.blob.command.bot.JoinCommand;
 import fr.ribesg.blob.command.bot.PartCommand;
@@ -17,11 +16,14 @@ import fr.ribesg.blob.command.minecraft.mcstats.GlobalMCStatsCommand;
 import fr.ribesg.blob.command.minecraft.mcstats.MCStatsCommand;
 import fr.ribesg.blob.command.util.GoogleCommand;
 import fr.ribesg.blob.command.util.ShortenCommand;
+import org.apache.log4j.Logger;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class BlobClient extends Client {
+
+	private static final Logger LOGGER = Logger.getLogger(BlobClient.class.getName());
 
 	public BlobClient() {
 		super("Blob");
@@ -30,18 +32,18 @@ public class BlobClient extends Client {
 	@Override
 	protected void load() {
 		// EsperNet
-		final Server esperNet = new Server(this, "irc.esper.net", 6697, SSLType.TRUSTING);
-		esperNet.addChannel("#alix");
-		esperNet.addChannel("#bendemPlugins");
+		final Server esperNet = new Server(this, getName(), "irc.esper.net", 6697, SSLType.TRUSTING);
+//		esperNet.addChannel("#alix");
+//		esperNet.addChannel("#bendemPlugins");
 		esperNet.addChannel("#blob");
-		esperNet.addChannel("#drtshock");
-		esperNet.addChannel("#ncube");
-		esperNet.addChannel("#ribesg");
+//		esperNet.addChannel("#drtshock");
+//		esperNet.addChannel("#ncube");
+//		esperNet.addChannel("#ribesg");
 		this.getServers().add(esperNet);
 
-		final Server freenode = new Server(this, "chat.freenode.net", 6697, SSLType.TRUSTING);
-		freenode.addChannel("#brainjar");
-		this.getServers().add(freenode);
+//		final Server freenode = new Server(this, getName(), "chat.freenode.net", 6697, SSLType.TRUSTING);
+//		freenode.addChannel("#brainjar");
+//		this.getServers().add(freenode);
 
 		final Set<String> admins = new HashSet<>();
 		admins.add("Ribesg");
@@ -71,15 +73,11 @@ public class BlobClient extends Client {
 	}
 
 	@Override
-	public void switchToBackupName() {
-		this.name = this.getName().substring(0, this.getName().length() - 1) + 'o' + this.getName().substring(this.getName().length() - 1);
-		this.getServers().stream().filter(Server::hasJoined).forEach(server -> server.send(new NickIrcPacket(this.name)));
-	}
-
-	@Override
 	public void onClientJoinChannel(final Channel channel) {
 		// Anti-shitty Willie
+		LOGGER.debug("DEBUG: Updating users...");
 		channel.updateUsers(true);
+		LOGGER.debug("DEBUG: Users updated!");
 		if (channel.getUserNames().contains("Willie")) {
 			channel.sendMessage("Hey " + Codes.RED + "Willie" + Codes.RESET + ", don't kick me, stupid bot, thanks!");
 		}
